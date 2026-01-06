dcb81a8e3a3a026d6137a45a05deca425a430559
alexb821 <alex.b821@gmail.com>
2026-01-06 10:44:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const lat = 34.05;
 const lon = -118.25;
 
-const url = `https://api.open-meteo.com/v1/forecast
-  ?latitude=${lat}
-  &longitude=${lon}
-  &current_weather=true
-  &daily=temperature_2m_max,temperature_2m_min
-  &timezone=auto`;
+const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&daily=temperature_2m_max,temperature_2m_min&timezone=auto`;
 
 fetch(url)
   .then(res => res.json())
